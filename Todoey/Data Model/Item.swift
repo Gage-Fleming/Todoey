3dd92ee7be6661e5fb4204d10b5f2d3da0ac1fa1
Gage Fleming <Gage.Fleming@Outlook.com>
2018-02-14 00:56:51 -0700

    Add model, view, controller segregation

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,16 @@
+//
+//  DataModel.swift
+//  Todoey
+//
+//  Created by Gage Fleming on 2018-02-14.
+//  Copyright © 2018 Gage Fleming. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+}
